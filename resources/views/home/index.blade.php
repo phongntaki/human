eb473218e166ebc27b4ae62ee18d54637738625a
株式会社エンジ <phongntaki@gmail.com>
2019-01-07 11:31:13 +0900

    スライダーに日時を追加

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -19,6 +19,7 @@
                     <div class="item">
                         <a href="{{url('/chi-tiet/'.$item_km->slug)}}">
                             <div class="item-lead">
+                                <h3 class="item-desc">{{ $item_km->created_at->format('Y/m/d')}}</h3>
                                 <h3 class="item-title">{{ $item_km->newsname}}</h3>
                                 <p class="item-desc">{{ $item_km->newintro}}</p>
                             </div>
